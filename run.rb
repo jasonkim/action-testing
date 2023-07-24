1b59e8b31c6318e3db6f48f276a36b3ce446a9a6
Jason Kim <jasonkim@users.noreply.github.com>
2023-07-23 23:26:03 -0700

    Update run.rb

--- run.rb
+++ run.rb
@@ -1,4 +1,4 @@
 #! /usr/bin/env ruby
 
-puts "Var from ruby: " + ENV['var']
-puts "Secret from ruby: " + ENV['secret']
+puts "Var from ruby: #{ENV['var']}"
+puts "Secret from ruby: #{ENV['secret']}"
